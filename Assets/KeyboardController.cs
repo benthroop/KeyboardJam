0bf70b6678908b997af5de9d8d7c197bb75075bc
Ben Throop <benthroop@gmail.com>
2019-10-14 16:39:49 -0400

    Base implementation using transforms and a single controller. And a bunch of Dictionaries.

---
+++ Assets/KeyboardController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour
+{
+    public Transform[] keys;
+    public Dictionary<Transform, Vector3> keyStartingLocalPositions;
+    public Dictionary<KeyCode, Transform> keyKeyCodes;
+    private KeyCode[] allKeyCodes;
+    public Vector3 keyOffset = new Vector3(0f, 0f, 0.09f);
+
+    void Start()
+    {
+        allKeyCodes = System.Enum.GetValues(typeof(KeyCode)) as KeyCode[];
+        keyKeyCodes = new Dictionary<KeyCode, Transform>();
+        keyStartingLocalPositions = new Dictionary<Transform, Vector3>();
+
+        for (int i = 0; i < keys.Length; i++)
+        {
+            keyStartingLocalPositions.Add(keys[i], keys[i].localPosition);
+        }
+
+        //build a matching array of trimmed names to compare. assume that the key names are Key_<some keycode>
+        string[] trimmedKeyNames = new string[keys.Length];
+        for (int i = 0; i < trimmedKeyNames.Length; i++)
+        {
+            var spl = keys[i].name.Split('_');
+            if (spl.Length == 2)
+            {
+                trimmedKeyNames[i] = spl[1];
+            }
+            else
+            {
+                Debug.LogError("Malformed key name. Expected Key_<name>. Using entire name " + keys[i].name + "as fallback.");
+                trimmedKeyNames[i] = keys[i].name;
+            }
+        }
+
+        //build the dictionary so we can quickly match keycode to transform in the update
+        string[] n = System.Enum.GetNames(typeof(KeyCode));
+        int e = n.Length;
+        for (int i = 0; i < e; i++)
+        {
+            for (int k = 0; k < trimmedKeyNames.Length; k++)
+            {
+                if (trimmedKeyNames[k] == n[i])
+                {
+                    keyKeyCodes.Add(allKeyCodes[i], keys[k]);
+                }
+            }
+        }
+    }
+
+    private Transform tmpKey;
+    void Update()
+    {
+        for (int i=0; i < allKeyCodes.Length; i++)
+        {
+            if (Input.GetKeyDown(allKeyCodes[i]))
+            {
+                if (keyKeyCodes.TryGetValue(allKeyCodes[i], out tmpKey))
+                {
+                    tmpKey.localPosition = keyStartingLocalPositions[tmpKey] - keyOffset;
+                }
+            }
+            if (Input.GetKeyUp(allKeyCodes[i]))
+            {
+                if (keyKeyCodes.TryGetValue(allKeyCodes[i], out tmpKey))
+                {
+                    tmpKey.localPosition = keyStartingLocalPositions[tmpKey];
+                }
+            }
+        }
+    }
+
+    KeyCode FetchKey()
+    {
+        var e = System.Enum.GetNames(typeof(KeyCode)).Length;
+        for (int i = 0; i < e; i++)
+        {
+            if (Input.GetKey((KeyCode)i))
+            {
+                return (KeyCode)i;
+            }
+        }
+
+        return KeyCode.None;
+    }
+}
